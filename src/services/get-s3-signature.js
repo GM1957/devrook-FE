4ce8d58f955a3b5cb722328195e87cf2c4c6bb07
GM1957 <tanmoybhowmik03@gmail.com>
2021-06-08 19:35:00 +0530

    s3 bucket name change for production

--- src/services/get-s3-signature.js
+++ src/services/get-s3-signature.js
@@ -3,7 +3,10 @@ const uuid = require("uuid");
 
 const getS3Signeture = async (name) => {
   const result = await axios.post(apis.S3_GET_PRESIGNED_URL, {
-    bucket: "dev-devrook-be-main-bucket",
+    bucket:
+      process.env.NODE_ENV === "production"
+        ? "prod-devrook-be-main-bucket"
+        : "dev-devrook-be-main-bucket",
     directory: "posts",
     fileName: uuid.v4() + name,
   });
